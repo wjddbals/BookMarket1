e1efe2fef042824f177baac3a78fd271bf83e997
FULLSTACK <jumin0220@naver.com>
2023-06-01 18:50:05 +0900

    localhost:3030/home화면 구현

--- src/main/java/jiho/bookmarket/bookmarket/controller/WelcomeController.java
+++ src/main/java/jiho/bookmarket/bookmarket/controller/WelcomeController.java
@@ -2,12 +2,15 @@ package jiho.bookmarket.bookmarket.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class WelcomeController {
-    @RequestMapping(value = "/home",method = RequestMethod.GET)
+@RequestMapping("/")
+public class WelcomeController { //http://localhost:3000/home
+
+  @GetMapping(value = "/home")
     public  String welcome(Model model) {
         model.addAttribute("greeting","welcome to BookMarket");
         model.addAttribute("strapline","welcome to Web Shopping Mall");
---
+++ src/main/java/jiho/bookmarket/bookmarket/model/Book.java
@@ -0,0 +1,14 @@
+package jiho.bookmarket.bookmarket.model;
+
+public class Book {
+    private String bookId;
+    private String name;
+    private int unitPrice;
+    private String author;
+    private String description;
+    private String publisher;
+    private String category;
+    private long unitInStock;
+    private String releaseDate;
+    private String condition;
+}
