56d082b6d4a045f5661141fc030a330173eea346
FULLSTACK <jumin0220@naver.com>
2023-06-01 17:55:50 +0900

    Initial commit-1

---
+++ src/main/java/jiho/bookmarket/bookmarket/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package jiho.bookmarket.bookmarket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WelcomeController {
+    @RequestMapping(value = "/home",method = RequestMethod.GET)
+    public  String welcome(Model model) {
+        model.addAttribute("greeting","welcome to BookMarket");
+        model.addAttribute("strapline","welcome to Web Shopping Mall");
+        return "welcome";
+
+    }
+
+
+}
